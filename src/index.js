0856291a478d7605eec5f13012e48d2a692c0e86
Владимир Зубович <moraviin@gmail.com>
2018-09-26 10:13:05 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,27 +1,36 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.initialValue = initialValue;
   }
 
   add(number) {
-    // your implementation
+   this.initialValue += '+' + number;
+   return this;
   }
   
   subtract(number) {
-    // your implementation
+     this.initialValue += '-' + number;
+    return this;
   }
 
   multiply(number) {
-    // your implementation
-  }
+     this.initialValue += '*' + number;
+     return this;
+    }
 
   devide(number) {
-    // your implementation
-  }
+     this.initialValue += '/' + number;
+     return this;
+    }
 
   pow(number) {
-    // your implementation
-  }
+     this.initialValue += '**' + number; //ES16  exponentiation operator 
+     return this;
+    }
+
+  toString(){
+      return eval(this.initialValue);
+    }
 }
 
 module.exports = SmartCalculator;
